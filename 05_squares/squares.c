cf00befe8cd312bd42958bd429b6ac8417496eee
Coursera Learner <nobody@nowhere.nul>
2018-07-23 16:38:52 +0000

    resubmit assignment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -21,14 +21,14 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     for (int x = 0; x < w; x++){
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND
-      if ((x >= x_offset) && (x <= (x_offset + size2)) &&
+      if (((( x >= x_offset) && (x <= (x_offset + size2)))&&
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
-	 ((y == y_offset) || (y == (y_offset + size2 -1))) ||
+	   ((y == y_offset) || (y == (y_offset + size2 -1)))) ||
 	  //OR
       //    ((y is between y_offset and y_offset + size2) AND
-	((y >= y_offset) && (y <= (y_offset + size2))) &&
+	  (((y >= y_offset) && (y <= (y_offset + size2))) &&
     //     x is equal to either x_offset OR x_offset + size2 -1)
-	((x == x_offset) || (x == (x_offset + size2 -1)))){
+	   ((x == x_offset) || (x == (x_offset + size2 -1))))){
       // if so, print a *
 	    printf("*");
 	 }
@@ -56,7 +56,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
   }
 }
 
-void main(int argc, char* argv[]){
+int main(int argc, char* argv[]){
   int a,b,c,d;
   a = atoi(argv[1]);
   b = atoi(argv[2]);
